9c0673f0d423a55d281385b21f1cf2d5023eaa91
sadramoh <shockylemi@gmail.com>
2026-06-20 17:43:13 +0200

    fix(runtime): remove runtime

--- runtime/src/scope/scope_item.rs
+++
@@ -1,8 +0,0 @@
-use crate::Value;
-
-/// The different types of items that can be stored in a scope.
-#[derive(Debug)]
-pub enum ScopeItem {
-    Variable { value: Value, is_mutable: bool },
-    Function,
-}
